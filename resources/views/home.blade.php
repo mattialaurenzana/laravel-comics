04eca2e1da25458b7e126a1d506b8797a9ff3307
mattia laurenzana <mattilaure@gmail.com>
2022-03-03 17:37:00 +0100

    Header completed

---
+++ resources/views/home.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Dc Comics</title>
+</head>
+<body>
+    
+    <div class="pre-header">
+       <div class="pre-header-content">
+            <span>DC POWER VISA®</span>
+           <span>ADDITIONAL DC SITES <i class="fas fa-caret-down"></i></span>
+       </div>
+    </div>
+
+{{-- header della pagina --}}
+    <header>
+
+
+        <div class="header-content">
+            <div class="header-logo">
+                <img src="{{asset('images/dc-logo.png')}}" alt="logo">
+            </div>
+    
+            <ul class="list-container">
+                <li><a href="">CHARACTERS</a></li>
+                <li><a href="">COMICS</a></li>
+                <li><a href="">MOVIES</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">GAMES</a></li>
+                <li><a href="">COLLECTIBLES</a></li>
+                <li><a href="">VIDEOS</a></li>
+                <li><a href="">FANS</a></li>
+                <li><a href="">NEWS</a></li>
+                <li><a href="">SHOP</a></li>
+                <li class="input-container">
+                    <input type="text" placeholder="Search">
+                    <i class="fas fa-search"></i>
+                </li>
+                
+            </ul>
+        </div>
+
+        
+        
+    </header>
+
+
+
+</body>
+</html>
